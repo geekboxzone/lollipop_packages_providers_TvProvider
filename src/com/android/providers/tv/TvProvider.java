8497462c04c89dd06d4346a8104d524c420c4087
Dongwon Kang <dwkang@google.com>
2014-07-01 15:27:10 +0900

    Add COLUMN_CONDITIONAL_ACCESS in Channels table.
    Related change: I62d744b92e31fbbb95ba10b2595686114f6fbb6f
    
    Bug: 14073703
    Change-Id: If098208ce068f281e03cf8692a6865697067feb6

--- src/com/android/providers/tv/TvProvider.java
+++ src/com/android/providers/tv/TvProvider.java
@@ -67,7 +67,7 @@ public class TvProvider extends ContentProvider {
     private static final boolean DEBUG = true;
     private static final String TAG = "TvProvider";
 
-    private static final int DATABASE_VERSION = 8;
+    private static final int DATABASE_VERSION = 9;
     private static final String DATABASE_NAME = "tv.db";
     private static final String CHANNELS_TABLE = "channels";
     private static final String PROGRAMS_TABLE = "programs";
@@ -150,6 +150,8 @@ public class TvProvider extends ContentProvider {
                 CHANNELS_TABLE + "." + Channels.COLUMN_BROWSABLE);
         sChannelProjectionMap.put(Channels.COLUMN_SEARCHABLE,
                 CHANNELS_TABLE + "." + Channels.COLUMN_SEARCHABLE);
+        sChannelProjectionMap.put(Channels.COLUMN_CONDITIONAL_ACCESS,
+                CHANNELS_TABLE + "." + Channels.COLUMN_CONDITIONAL_ACCESS);
         sChannelProjectionMap.put(Channels.COLUMN_INTERNAL_PROVIDER_DATA,
                 CHANNELS_TABLE + "." + Channels.COLUMN_INTERNAL_PROVIDER_DATA);
         sChannelProjectionMap.put(Channels.COLUMN_VERSION_NUMBER,
@@ -235,6 +237,7 @@ public class TvProvider extends ContentProvider {
                     + Channels.COLUMN_DESCRIPTION + " TEXT,"
                     + Channels.COLUMN_BROWSABLE + " INTEGER NOT NULL DEFAULT 1,"
                     + Channels.COLUMN_SEARCHABLE + " INTEGER NOT NULL DEFAULT 1,"
+                    + Channels.COLUMN_CONDITIONAL_ACCESS + " INTEGER NOT NULL DEFAULT 0,"
                     + Channels.COLUMN_INTERNAL_PROVIDER_DATA + " BLOB,"
                     + CHANNELS_COLUMN_LOGO + " BLOB,"
                     + Channels.COLUMN_VERSION_NUMBER + " INTEGER"
